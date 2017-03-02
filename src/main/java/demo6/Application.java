8b81050ab42a2feaf9e5e5f2098c57f19c98f389
wuyifeng <qq736836427@gmail.com>
2017-03-02 14:01:48 +0800

    Spring依赖注入

--- src/main/java/demo6/Application.java
+++ src/main/java/demo6/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Application {
     public static void main(String[] args){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("demo6/beans.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("demo6/beans.xml" );
 
         A_B_Hellper a_b_hellper = (A_B_Hellper) applicationContext.getBean("Helper");
         a_b_hellper.printHello();
--- src/main/java/demo6/SayHello.java
+++ src/main/java/demo6/SayHello.java
@@ -3,6 +3,6 @@ package demo6;
 /**
  * Created by win8 on 2017/2/28.
  */
-public interface SayHello {
+public interface  SayHello {
     public void printHello();
 }
